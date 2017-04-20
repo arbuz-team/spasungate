1398be5c13190616e17bb9641f233675cb1e7468
arbuz93 <93.endo@gmail.com>
2017-04-20 19:14:59 +0200

    Little change.

--- server/main/views.py
+++ server/main/views.py
@@ -5,7 +5,7 @@ from server.arbuz.views import *
 class Start(Dynamic_Event_Manager):
 
     def Manage_Index(self):
-        return self.Render_HTML('main/start.html')
+        return self.Render_HTML('_page/start.html')
 
     @staticmethod
     def Launch(request):
