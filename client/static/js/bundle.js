22516562891341b1c3dd85010a8a3988e1d5f819
Drego31 <dominik.betka@arbuz.team>
2017-04-22 16:22:48 +0200

    Creating design.

--- client/static/js/bundle.js
+++ client/static/js/bundle.js
@@ -67,17 +67,32 @@
 /* 5 */
 /***/ function(module, exports) {
 
+	'use strict';
+	
 	/**
 	 * Created by mrskull on 24.11.16.
 	 */
 	
-	// import './autosize-master/dist/autosize';
-	// import './arbuz/js/base';
-	// import './arbuz/js/http_request';
-	// import * as page_controller from './arbuz/js/controllers';
-	//
-	// page_controller.start();
-	"use strict";
+	var set_ground_size = function set_ground_size() {
+		var $container = document.getElementById('CONTAINTER'),
+		    $header = $container.querySelectorAll('.header')[0],
+		    $ground = $container.querySelectorAll('.ground')[0],
+		    $ground_block = $container.querySelectorAll('.ground-block')[0],
+		    $footer = $container.querySelectorAll('.footer-content')[0],
+		    window_height = window.document.body.clientHeight,
+		    header_height = $header.clientHeight,
+		    footer_height = $footer.clientHeight,
+		    ground_before_height = $ground.offsetTop,
+		    ground_height = window_height - header_height,
+		    ground_block_height = ground_height - footer_height - ground_before_height;
+	
+		$ground.style.height = ground_height + 'px';
+		$ground_block.style.minHeight = ground_block_height + 'px';
+	};
+	
+	window.addEventListener('load', set_ground_size);
+	
+	window.addEventListener('resize', set_ground_size);
 
 /***/ }
 /******/ ]);
