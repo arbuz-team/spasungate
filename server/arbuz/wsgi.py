fbb65e64d0b4a1f445287d5226294cd899c9cb8e
arbuz93 <93.endo@gmail.com>
2017-05-21 21:25:22 +0200

    Release

--- server/arbuz/wsgi.py
+++ server/arbuz/wsgi.py
@@ -11,7 +11,7 @@ import os, sys
 from django.core.wsgi import get_wsgi_application
 
 sys.path.append('/home/internet/spasungate')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "arbuz.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.arbuz.settings")
 
 application = get_wsgi_application()
 
